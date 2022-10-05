e79b01792a5c62d3729b1fcab6db3814435511da
Tanner Smiley <96635819+The-Underground-Club@users.noreply.github.com>
2022-10-04 19:48:03 -0500

    Add files via upload

---
+++ TCP-chat/server.py
@@ -0,0 +1,55 @@
+import threading
+import socket
+
+host = '127.0.0.1'  # localhost
+port = 8000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except Exception as e:
+            print(e)
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f"{nickname} left the chat!".encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send("NICK".encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nick of the client is {nickname}!')
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send("Connected to the server".encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server is listening...")
+receive()
